aa7926d910fdc0f29bc0fb7c8970bfd89c65ed60
andre mcmorris <ajmcmorris@gmail.com>
2025-04-29 11:32:09 -0400

    small enhancements

--- src/app/core/components/media/media.component.ts
+++ src/app/core/components/media/media.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, Input, ViewContainerRef } from '@angular/core';
 import { MoviesService } from '../../services/movies/movies.service';
-import { filter, SubscriptionLike } from 'rxjs';
+import { filter, Subject, SubscriptionLike, takeUntil } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { selectAllCollections } from '../../../state/state.selectors';
 import { CommonModule } from '@angular/common';
@@ -16,10 +16,10 @@ import { Media } from '../../../shared/models/media.model';
 export class MediaComponent {
   private store = inject(Store);
   collections$ = this.store.select(selectAllCollections);
-  subscription: SubscriptionLike[] = [];
   media!: Media;
   @Input() movieTitle = '';
   showDropdown = false;
+  destroy$ = new Subject<void>();
   constructor(private movies: MoviesService, public vcr: ViewContainerRef) {}
 
   ngOnInit() {
@@ -50,28 +50,26 @@ export class MediaComponent {
       console.log('No text to search for');
       return;
     }
-    this.subscription.push(
-      this.movies
-        .getMovies(text)
-        .pipe(
-          filter((data: Media) => {
-            if (data.Response === 'True') {
-              return true;
-            } else {
-              console.log('No movies found');
-              return false;
-            }
-          })
-        )
-        .subscribe((data: Media) => {
-          this.media = data;
-          console.log('Movies found', data);
+    this.movies
+      .getMovies(text)
+      .pipe(
+        takeUntil(this.destroy$),
+        filter((data: Media) => {
+          if (data.Response === 'True') {
+            return true;
+          } else {
+            console.log('No movies found');
+            return false;
+          }
         })
-    );
+      )
+      .subscribe((data: Media) => {
+        this.media = data;
+      });
   }
 
   ngOnDestroy() {
-    console.log('MoviesComponent destroyed');
-    this.subscription.forEach((sub) => sub.unsubscribe());
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
